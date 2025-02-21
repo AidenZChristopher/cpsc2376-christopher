e81bf29fc0ab2d890983c5687f4e33c630a35f7a
AidenZChristopher <azchristophe@ualr.edu>
2025-02-21 14:42:29 -0600

    practice03 startup created - followed zoom video

---
+++ practice/practice03/mathStuff.cpp
@@ -0,0 +1,16 @@
+#include "mathStuff.h"
+
+int add(int x, int y)
+{
+	return x + y;
+}
+
+int doubleValue(int x)
+{
+	return 2 * x;
+}
+
+int Rectangle::area()
+{
+	return width * height;
+}
---
+++ tests/pactice03_test/practice03_test.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+//#include "../../practice/practice03/mathStuff.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace pactice03test
+{
+	TEST_CLASS(pactice03test)
+	{
+	public:
+		
+		/*TEST_METHOD(TestAdd1)
+		{
+			Assert::AreEqual(5, add(2, 3));
+		}
+
+		TEST_METHOD(TestDouble1)
+		{
+			Assert::AreEqual(10, doubleValue(5));
+		}
+
+		TEST_METHOD(TestFancy1)
+		{
+			Rectangle r{ 3, 4 };
+			r.width++;
+
+			Assert::AreEqual(16, r.area());
+			Assert::AreEqual(4, r.width);
+		}*/
+
+
+	};
+}
