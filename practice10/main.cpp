25e9b89a0af0579a1279992941c02feceae79ae6
AidenZChristopher <azchristophe@ualr.edu>
2025-04-23 22:11:53 -0500

    practice 10
    finished practice 10

---
+++ practice10/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <memory>
+#include <string>
+#include <iomanip>
+const double pi = 3.14159;
+
+class Shape
+{
+public:
+	virtual double getArea() const = 0;
+	virtual ~Shape() = default;
+};
+
+class Rectangle : public Shape
+{
+private:
+	double width;
+	double height;
+public:
+	Rectangle(double width, double height) : width(width), height(height) {}
+	double getArea() const override { return width * height; }
+
+};
+
+class Circle : public Shape
+{
+private:
+	double radius;
+public:
+	Circle(double radius) : radius(radius) {}
+	double getArea() const override { return pi * radius * radius; }
+
+};
+
+void printAllAreas(const std::vector<std::unique_ptr<Shape>>& shapes)
+{
+	for (const std::unique_ptr<Shape>& shape : shapes)
+	{
+		std::cout << "Area: " << shape->getArea() << "\n";
+	}
+}
+
+
+int main()
+{
+	std::ifstream file("shapes.txt");
+	if (!file) {
+		std::cerr << "Error opening shapes.txt\n";
+		return 1;
+	}
+
+	std::vector<std::unique_ptr<Shape>> shapes;
+	std::string type;
+	while (file >> type)
+	{
+		if (type == "rectangle")
+		{
+			double width, height;
+			file >> width >> height;
+			auto rectPtr = std::make_unique<Rectangle>(width, height);
+			shapes.push_back(std::move(rectPtr));
+		}
+		else if (type == "circle")
+		{
+			double radius;
+			file >> radius;
+			auto circPtr = std::make_unique<Circle>(radius);
+			shapes.push_back(std::move(circPtr));
+		}
+	}
+	printAllAreas(shapes);
+}
