a2a5e078112595cfb1802bd47dcd912e14c8fd86
AidenZChristopher <azchristophe@ualr.edu>
2025-04-25 11:20:51 -0500

    started practice 11

--- practice/practice10/main.cpp
+++ practice/practice10/main.cpp
@@ -1,3 +1,12 @@
+/*
+This code may have been developed with the assistance of artificial intelligence (AI) tools,
+which were utilized to consolidate, edit, or refine the content.
+However, these tools were employed solely to enhance clarity, efficiency, or structure.
+All concepts, logic, and problem-solving approaches were originally conceived, reviewed, and fully understood by me,
+ensuring that I can ethically and confidently claim this code as my own work.
+AI tools were used as supplementary resources and did not replace my involvement or understanding in any aspect of the code's creation.
+I am responsibility for this code's functionality and accuracy rests entirely with me.
+*/
 #include <iostream>
 #include <fstream>
 #include <vector>
---
+++ practice/practice11/design.md
@@ -0,0 +1,13 @@
+STEP 1
+- A program that tracks board games in use. The program needs to keep track 
+of what is currently avaiable, checkout copies to customers, track which 
+customers has what,and track when games are returned.
+
+Step 2
+- Nouns: Boardgame, Customer, Inventory, 
+- Attributes:
+	- boardgame: Title, In use
+	- customer: boardgame, Return Date
+	- invetory: boardgame copies
+- Verbs:
+	- 
