1ff53edfef9c6e574477be8cdd117344395fda06
AidenZChristopher <azchristophe@ualr.edu>
2025-05-05 19:03:05 -0500

    finished practice04

--- projects/Project04/main.cpp
+++ projects/Project04/main.cpp
@@ -6,12 +6,15 @@
 
 int main() {
     Engine engine("Connect Four", 700, 700,
-        "Ubuntu-Bold.ttf",  // this must match your file’s name
+        "Ubuntu-Bold.ttf",
         24,
         "move.wav");
     Game game;
     bool running = true;
     int selectedCol = 0;
+    int scoreX = 0;
+    int scoreO = 0;
+    Game::Status previousStatus = Game::Status::Running;
 
     while (running) {
         SDL_Event e;
@@ -34,6 +37,7 @@ int main() {
                 else if (key == SDLK_r) {
                     game.reset();
                     selectedCol = 0;
+                    previousStatus = Game::Status::Running;
                 }
                 else if (key == SDLK_ESCAPE) {
                     running = false;
@@ -47,24 +51,38 @@ int main() {
                 }
             }
         }
+
+        Game::Status status = game.status();
+        if (previousStatus == Game::Status::Running && status == Game::Status::XWin) {
+            scoreX++;
+        }
+        if (previousStatus == Game::Status::Running && status == Game::Status::OWin) {
+            scoreO++;
+        }
+        previousStatus = status;
+
         engine.clear(SDL_Color{ 0, 0, 0, 255 });
         game.draw(engine);
 
-        if (game.status() == Game::Status::XWin) {
-            engine.drawText("X Wins!", 350, 50, SDL_Color{ 255, 255, 255, 255 });
+        engine.drawText("Player 1: " + std::to_string(scoreX), 70, 10, SDL_Color{ 255, 0, 0, 255 });
+        engine.drawText("Player 2: " + std::to_string(scoreO), 610, 10, SDL_Color{ 255, 255, 0, 255 });
+
+        if (status == Game::Status::XWin) {
+            engine.drawText("Player 1 Wins!", 350, 50, SDL_Color{ 255, 255, 255, 255 });
         }
-        else if (game.status() == Game::Status::OWin) {
-            engine.drawText("O Wins!", 350, 50, SDL_Color{ 255, 255, 255, 255 });
+        else if (status == Game::Status::OWin) {
+            engine.drawText("Player 2 Wins!", 350, 50, SDL_Color{ 255, 255, 255, 255 });
         }
-        else if (game.status() == Game::Status::Draw) {
+        else if (status == Game::Status::Draw) {
             engine.drawText("Draw!", 350, 50, SDL_Color{ 255, 255, 255, 255 });
         }
 
         engine.drawCircle(selectedCol * 100 + 50, 50, 15, SDL_Color{ 255, 255, 255, 255 });
-        engine.drawText("Press R to restart or Esc to quit", 350, 680, SDL_Color{ 255, 255, 255, 255 });
+        engine.drawText("Press R to restart or Esc to quit", 340, 10, SDL_Color{ 255, 255, 255, 255 });
         engine.flip();
         SDL_Delay(16);
     }
 
     return 0;
 }
+
